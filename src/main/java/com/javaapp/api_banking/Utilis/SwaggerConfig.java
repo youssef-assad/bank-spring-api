7b3180d05829bd11e50c985a80f9db200853078c
youssef-assad <youssefassad158@gmail.com>
2026-02-04 13:26:43 +0100

    ajouter Swagger ui

---
+++ src/main/java/com/javaapp/api_banking/Utilis/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.javaapp.api_banking.Utilis;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bank API")
+                        .version("1.0")
+                        .description("API sécurisée pour la gestion bancaire avec JWT et Refresh Token"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+}
